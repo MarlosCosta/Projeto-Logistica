2ee6bd6d01db26b87e075890e93663785e962c17
Marlos Costa <marlos@outlook.com>
2023-05-22 10:09:27 -0300

    Tratativa do erro ValueError e ajuste da Identação - Programa finalizado

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
 #Atividade 03 de 04 do Trabalho
-#Inicio da função dimensoesObjeto()
+#Inicio da função obter_dimensoes_objeto()
 def obter_dimensoes_objeto():
-    print("-----Dimensões do Objeto-----")
+    print("-----Cadastro das Dimensões do Objeto-----")
     while True:
-        altura = input("Digite o altura do objeto (em cm): ")
-        comprimento = input("Digite o comprimento do objeto (em cm): ")
-        largura = input("Digite a largura do objeto (em cm): ")
-
-        # Valida se os valores digitados são numéricos e maiores que zero
-        if not altura.isdigit() or not comprimento.isdigit() or not largura.isdigit():
+        #Tratativa do erro caso usuario entre com valor não numerico
+        try:
+            altura = float(input("Digite o altura do objeto (em cm): "))
+            comprimento = float(input("Digite o comprimento do objeto (em cm): "))
+            largura = float(input("Digite a largura do objeto (em cm): "))
+        except ValueError:
             print("Você digitou alguma dimensão do objeto com valor não numérico!")
+            print("Digite as dimensões novamente")
             continue
-        else:#Converte a entrada do usuário em float
-            altura = float(altura)
-            comprimento = float(comprimento)
-            largura = float(largura)
 
-            # Verifica se algum valor é menor que zero
-            if altura <= 0 or comprimento <= 0 or largura <= 0:
-                print("Você digitou alguma dimensão do objeto com valor igual ou abaixo de zero!")
-                continue
+        # Verifica se algum valor é menor que zero
+        if altura <= 0 or comprimento <= 0 or largura <= 0:
+            print("Você digitou alguma dimensão do objeto com valor igual ou abaixo de zero!")
+            continue
 
         # Cálculo do volume
         volume_objeto = altura * comprimento * largura
@@ -42,21 +39,23 @@ def obter_dimensoes_objeto():
 
     #Retorna a variavel para ser usada fora da função
     return valor_objeto
-#Fim da função dimensoesObjeto()
-
-#Inicio da função pesoObjeto()
+#Fim da função obter_dimensoes_objeto()
+#Inicio da função obter_peso_objeto()
 def obter_peso_objeto():
     print()#Pula uma linha para que a saida no console fique mais legivel
-    print("-----Peso do Objeto-----")
+    print("-----Cadastro do Peso do Objeto-----")
     while True:
-        peso = input("Digite o peso do objeto (em kg): ")
-
-        # Valida se o valor digitado é numérico e maior que zero
-        if not peso.isdigit() or float(peso) <= 0:
-            print("Você digitou um peso com valor não numérico ou igual/abaixo de zero!")
+        try:
+            peso = float(input("Digite o peso do objeto (em kg): "))
+        except ValueError:
+            print("Você digitou o peso do objeto com valor não numérico!")
+            print("Digite o peso novamente")
             continue
 
-        peso = float(peso) #converte o valor em float
+        if peso <= 0:
+            print("Você digitou o peso menor/igual a zero!")
+            print("Digite o peso novamente")
+            continue
 
         #calcula o multiplicador com base no peso digitado
         if peso <= 0.1:
@@ -77,12 +76,12 @@ def obter_peso_objeto():
     return multiplicador_peso #Retorna a variavel para ser usada fora da função
 
     print("O peso digitado foi de: {:.2f}kg".format(peso))
-#Fim da função pesoObjeto()
+#Fim da função obter_peso_objeto()
 
-#Inicio da função rotaObjeto()
+#Inicio da função obter_rota_objeto()
 def obter_rota_objeto():
     print()#Pula uma linha para que a saida no console fique mais legivel
-    print("-----Rota do Objeto-----")
+    print("-----Seleção da Rota do Objeto-----")
     while True:
         print("Rotas disponiveis:")
         print("PRSP - De Paraná para São Paulo ")
@@ -109,7 +108,7 @@ def obter_rota_objeto():
         break
 
     return multiplicador_rota #Retorna a variavel para ser usada fora da função
-#Fim da função rotaObjeto()
+#Fim da função obter_rota_objeto()
 
 
 #inicio do Main
@@ -118,9 +117,10 @@ print("Bem Vindo a Companhia Logistica Marlos Augusto da Costa S.A")
 
 #Recebe a variavel de retorno da função e aplica na variavel de mesmo nome fora da função
 valor_objeto = obter_dimensoes_objeto()
-multiplicador_peso  = obter_peso_objeto()
+multiplicador_peso = obter_peso_objeto()
 multiplicador_rota = obter_rota_objeto()
 
 #Variavel criada para calcular o valor final e apresentar o resultado com base nos dados fornecidos pelo usuario
-valor_final = valor_objeto * multiplicador_peso  * multiplicador_rota
-print("O valor final é de: R${:.2f}" .format(valor_final))
+valor_final = valor_objeto * multiplicador_peso * multiplicador_rota
+print()
+print("O valor final é de: R${:.2f} (dimensões: R${} * peso: {} * rota: {}" .format(valor_final, valor_objeto, multiplicador_peso, multiplicador_rota))
